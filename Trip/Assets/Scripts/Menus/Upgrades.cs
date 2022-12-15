228ed1fe86a0663c55c7d1082866deda136d5b95
McK-4 <117216683+McK-4@users.noreply.github.com>
2022-12-15 13:43:39 -0800

    Start of Upgrades

---
+++ Trip/Assets/Scripts/Menus/Upgrades.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    PlayerController Player;
+    //int bought;
+
+    /*
+     eG = Exponential growth
+     iA = intitial amount
+     gR = growth rate
+    */
+    float eG;
+    float iA;
+    float gR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire_Rate()
+    {
+        /*
+         iA = 1
+         gR = 0.2f;
+         */
+        Player.fireRate -= (Player.fireRate * 0.2f);
+    }
+
+    private void Exponential_Grow()
+    {
+        eG = iA * (1 + gR);
+    }
+}
